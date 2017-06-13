9b186bd4df5815948ee5498f15edd121a4d1454f
andela-ydaniju <yusuf.daniju@andela.com>
2017-06-13 11:03:11 +0100

    adder app

---
+++ app/src/main/java/com/example/ydaniju/firstapp/MyWorker.java
@@ -0,0 +1,11 @@
+package com.example.ydaniju.firstapp;
+
+/**
+ * Created by ydaniju on 13/06/2017.
+ */
+
+public class MyWorker {
+    public static int doubleTheValue(int value) {
+        return value * 2;
+    }
+}
